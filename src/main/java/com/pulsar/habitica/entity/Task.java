b654eaa9035b7c2a5928aca05781aa88c4ead99f
Кузичев Александр <pulsarmn@yandex.ru>
2024-08-10 19:42:21 +0300

    Add: implement the Task class

---
+++ src/main/java/com/pulsar/habitica/entity/Task.java
@@ -0,0 +1,19 @@
+package com.pulsar.habitica.entity;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class Task {
+
+    private Integer id;
+    private String heading;
+    private String description;
+    private Complexity complexity;
+    private LocalDate deadline;
+    private Boolean status;
+    private Integer userId;
+}
