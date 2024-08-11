7408abac96b5e437b9a0bfec575f91d9074e2f52
Кузичев Александр <pulsarmn@yandex.ru>
2024-08-11 20:00:05 +0300

    Add: implement the utility UserTable class

---
+++ src/main/java/com/pulsar/habitica/dao/UserTable.java
@@ -0,0 +1,14 @@
+package com.pulsar.habitica.dao;
+
+public final class UserTable {
+
+    public static final String SCHEMA_NAME = "users";
+    public static final String TABLE_NAME = "users";
+    public static final String FULL_TABLE_NAME = SCHEMA_NAME + "." + TABLE_NAME;
+
+    public static final String ID_COLUMN = "id";
+    public static final String EMAIL_COLUMN = "email";
+    public static final String PASSWORD_COLUMN = "password";
+    public static final String NICKNAME_COLUMN = "nickname";
+    public static final String CREATED_AT_COLUMN = "created_at";
+}
