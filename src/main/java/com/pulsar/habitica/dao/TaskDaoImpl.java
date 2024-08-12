d79b39fbba0f579af0009ff738fc852d0ad9dc9b
Кузичев Александр <pulsarmn@yandex.ru>
2024-08-12 13:29:22 +0300

    Add: implement the findAll() method

--- src/main/java/com/pulsar/habitica/dao/TaskDaoImpl.java
+++ src/main/java/com/pulsar/habitica/dao/TaskDaoImpl.java
@@ -1,20 +1,37 @@
 package com.pulsar.habitica.dao;
 
 
+import com.pulsar.habitica.entity.Complexity;
 import com.pulsar.habitica.entity.Task;
+import com.pulsar.habitica.util.ConnectionManager;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class TaskDaoImpl implements TaskDao {
 
+    private static final String FIND_ALL_SQL = "SELECT * FROM task.task";
     private static final TaskDaoImpl INSTANCE = new TaskDaoImpl();
 
     private TaskDaoImpl() {}
 
     @Override
     public List<Task> findAll() {
-        return null;
+        try (var connection = ConnectionManager.get();
+        var statement = connection.prepareStatement(FIND_ALL_SQL)) {
+            var resultSet = statement.executeQuery();
+            List<Task> tasks = new ArrayList<>();
+            while (resultSet.next()) {
+                Task task = buildTask(resultSet);
+                tasks.add(task);
+            }
+            return tasks;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -45,6 +62,17 @@ public class TaskDaoImpl implements TaskDao {
     @Override
     public List<Task> findByHeading(String heading) {
         return null;
+    }
+
+    private Task buildTask(ResultSet resultSet) throws SQLException {
+        return Task.builder()
+                .id(resultSet.getInt("id"))
+                .heading(resultSet.getString("heading"))
+                .description(resultSet.getString("description"))
+                .complexity(Complexity.valueOf(resultSet.getString("complexity")))
+                .deadline(resultSet.getDate("deadline").toLocalDate())
+                .status(resultSet.getBoolean("status"))
+                .build();
     }
 
     public static TaskDaoImpl getInstance() {
