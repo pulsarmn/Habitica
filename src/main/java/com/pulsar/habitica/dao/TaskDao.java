87b60bac6ba53af4bdfb66d199943f39bc9b5294
Кузичев Александр <pulsarmn@yandex.ru>
2024-08-12 21:04:13 +0300

    Refactor: redisign the TaskDao interface

--- src/main/java/com/pulsar/habitica/dao/TaskDao.java
+++ src/main/java/com/pulsar/habitica/dao/TaskDao.java
@@ -1,13 +1,14 @@
 package com.pulsar.habitica.dao;
 
 import com.pulsar.habitica.entity.Task;
+import com.pulsar.habitica.entity.TaskBase;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface TaskDao extends Dao<Integer, Task> {
+public interface TaskDao<T extends TaskBase> extends Dao<Integer, T> {
 
-    List<Task> findByHeading(String heading);
+    List<T> findByHeading(String heading);
 
-    List<Task> findAllByUserId(Integer userId);
+    List<T> findAllByUserId(Integer userId);
 }
