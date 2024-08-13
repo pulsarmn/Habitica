21c57f661cd8907b4635de29ce2417161d38b4ba
Кузичев Александр <pulsarmn@yandex.ru>
2024-08-13 19:41:53 +0300

    Add: implement the findById() method

--- src/main/java/com/pulsar/habitica/dao/RewardDaoImpl.java
+++ src/main/java/com/pulsar/habitica/dao/RewardDaoImpl.java
@@ -3,24 +3,51 @@ package com.pulsar.habitica.dao;
 import com.pulsar.habitica.entity.Reward;
 import com.pulsar.habitica.util.ConnectionManager;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class RewardDaoImpl implements RewardDao {
 
+    private static final String FIND_ALL_SQL = "SELECT * FROM task.reward";
+    private static final String FIND_BY_ID_SQL = "SELECT * FROM task.reward WHERE id = ?";
     private static final RewardDaoImpl INSTANCE = new RewardDaoImpl();
 
     private RewardDaoImpl() {}
 
     @Override
     public List<Reward> findAll() {
-        return null;
+        try (var connection = ConnectionManager.get();
+             var statement = connection.prepareStatement(FIND_ALL_SQL)) {
+            var resultSet = statement.executeQuery();
+            List<Reward> rewards = new ArrayList<>();
+            while (resultSet.next()) {
+                Reward reward = buildReward(resultSet);
+                rewards.add(reward);
+            }
+            return rewards;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Optional<Reward> findById(Integer id) {
-        return Optional.empty();
+        try (var connection = ConnectionManager.get();
+        var statement = connection.prepareStatement(FIND_BY_ID_SQL)) {
+            statement.setInt(1, id);
+
+            var resultSet = statement.executeQuery();
+            Reward reward = null;
+            if (resultSet.next()) {
+                reward = buildReward(resultSet);
+            }
+            return Optional.ofNullable(reward);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -51,6 +78,16 @@ public class RewardDaoImpl implements RewardDao {
     @Override
     public List<Reward> findAllByUserId(Integer userId) {
         return null;
+    }
+
+    public Reward buildReward(ResultSet resultSet) throws SQLException {
+        return Reward.builder()
+                .id(resultSet.getInt("id"))
+                .heading(resultSet.getString("heading"))
+                .description(resultSet.getString("description"))
+                .cost(resultSet.getBigDecimal("cost"))
+                .userId(resultSet.getInt("user_id"))
+                .build();
     }
 
     public static RewardDaoImpl getInstance() {
