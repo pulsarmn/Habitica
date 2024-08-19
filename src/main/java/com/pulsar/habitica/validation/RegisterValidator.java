ba3d2e7b4b748029f95805b4af6b0335ffb28ab1
Кузичев Александр <pulsarmn@yandex.ru>
2024-08-19 17:18:09 +0300

    add: final UserDao field and constructor to RegisterValidator class

--- src/main/java/com/pulsar/habitica/validation/RegisterValidator.java
+++ src/main/java/com/pulsar/habitica/validation/RegisterValidator.java
@@ -1,8 +1,15 @@
 package com.pulsar.habitica.validation;
 
+import com.pulsar.habitica.dao.user.UserDao;
 import com.pulsar.habitica.dto.RegisterUserDto;
 
 public class RegisterValidator implements Validator<RegisterUserDto> {
+
+    private final UserDao userDao;
+
+    public RegisterValidator(UserDao userDao) {
+        this.userDao = userDao;
+    }
 
     @Override
     public ValidationResult isValid(RegisterUserDto object) {
