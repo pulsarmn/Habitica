9bba830bfd09585267237bf285a2c20a59a3c021
Кузичев Александр <pulsarmn@yandex.ru>
2024-08-19 17:19:36 +0300

    add: implement the singleton pattern in the RegisterUserMapper class

--- src/main/java/com/pulsar/habitica/mapper/RegisterUserMapper.java
+++ src/main/java/com/pulsar/habitica/mapper/RegisterUserMapper.java
@@ -5,8 +5,23 @@ import com.pulsar.habitica.entity.user.User;
 
 public class RegisterUserMapper implements Mapper<RegisterUserDto, User> {
 
+    private static RegisterUserMapper INSTANCE;
+
+    private RegisterUserMapper() {}
+
     @Override
     public User mapFrom(RegisterUserDto object) {
         return null;
+    }
+
+    public static RegisterUserMapper getInstance() {
+        if (INSTANCE == null) {
+            synchronized (RegisterUserMapper.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new RegisterUserMapper();
+                }
+            }
+        }
+        return INSTANCE;
     }
 }
