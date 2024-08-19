17ffbb7a8eb4c8f8f0027414785338430db9ba2b
Кузичев Александр <pulsarmn@yandex.ru>
2024-08-19 17:23:56 +0300

    add: final ValidationResult field to RegisterValidator

--- src/main/java/com/pulsar/habitica/validation/RegisterValidator.java
+++ src/main/java/com/pulsar/habitica/validation/RegisterValidator.java
@@ -5,14 +5,18 @@ import com.pulsar.habitica.dto.RegisterUserDto;
 
 public class RegisterValidator implements Validator<RegisterUserDto> {
 
+    private final ValidationResult validationResult;
     private final UserDao userDao;
 
     public RegisterValidator(UserDao userDao) {
+        validationResult = new ValidationResult();
         this.userDao = userDao;
     }
 
     @Override
     public ValidationResult isValid(RegisterUserDto object) {
-        return null;
+        if (userDao.findByNickname(object.getNickname()).isEmpty()) {
+            
+        }
     }
 }
