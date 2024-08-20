6319acd9ebe6e053631ba1c026dfb5b5efdb054c
Кузичев Александр <pulsarmn@yandex.ru>
2024-08-20 14:48:19 +0300

    add: neccessery dependencies to UserService

--- src/main/java/com/pulsar/habitica/service/UserService.java
+++ src/main/java/com/pulsar/habitica/service/UserService.java
@@ -2,17 +2,24 @@ package com.pulsar.habitica.service;
 
 import com.pulsar.habitica.dao.user.UserDao;
 import com.pulsar.habitica.dto.RegisterUserDto;
+import com.pulsar.habitica.dto.UserDto;
 import com.pulsar.habitica.entity.user.User;
+import com.pulsar.habitica.exception.ValidationException;
+import com.pulsar.habitica.mapper.Mapper;
+import com.pulsar.habitica.mapper.RegisterUserMapper;
+import com.pulsar.habitica.mapper.UserMapper;
+import com.pulsar.habitica.validation.RegisterValidator;
+import com.pulsar.habitica.validation.Validator;
 
 public class UserService {
 
-    private UserDao userDao;
+    private final UserDao userDao;
+    private final Validator<RegisterUserDto> validator;
+    private final Mapper<RegisterUserDto, User> registerUserMapper = RegisterUserMapper.getInstance();
+    private final Mapper<User, UserDto> userDtoMapper = UserMapper.getInstance();
 
     public UserService(UserDao userDao) {
         this.userDao = userDao;
-    }
-
-    public User create(RegisterUserDto userDto) {
-        return null;
+        this.validator = new RegisterValidator(userDao);
     }
 }
