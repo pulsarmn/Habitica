36832b8d3734c0e5344a6df63141d36fea189a25
Кузичев Александр <pulsarmn@yandex.ru>
2024-08-24 11:13:27 +0300

    add: create AuthorizationFilter class and implement the doPost() method

---
+++ src/main/java/com/pulsar/habitica/filter/AuthorizationFilter.java
@@ -0,0 +1,40 @@
+package com.pulsar.habitica.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter("/*")
+public class AuthorizationFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        var httpRequest = (HttpServletRequest) servletRequest;
+        var httpResponse = (HttpServletResponse) servletResponse;
+        var session = httpRequest.getSession(false);
+
+        boolean isLogged = (session != null && session.getAttribute("user") != null);
+
+        String requestURI = httpRequest.getRequestURI();
+        if (isLogged && PagePaths.getGuestPaths().contains(requestURI)) {
+            httpResponse.sendRedirect(PrivatePaths.HOME.getPath());
+        }else if (!isLogged && PagePaths.getPrivatePaths().contains(requestURI)) {
+            httpResponse.sendRedirect(GuestPaths.LOGIN.getPath());
+        }else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
