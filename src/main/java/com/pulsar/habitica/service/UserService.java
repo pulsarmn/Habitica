3be9e6ec8929d08c501ee241e087e3171879f59e
Кузичев Александр <pulsarmn@yandex.ru>
2024-08-31 17:09:06 +0300

    add: new implementation of the create() method
    - add: initialization block for creating additional services.

--- src/main/java/com/pulsar/habitica/service/UserService.java
+++ src/main/java/com/pulsar/habitica/service/UserService.java
@@ -2,7 +2,10 @@ package com.pulsar.habitica.service;
 
 import com.pulsar.habitica.dao.user.UserBalanceDaoImpl;
 import com.pulsar.habitica.dao.user.UserDao;
+import com.pulsar.habitica.dao.user.UserImageDaoImpl;
+import com.pulsar.habitica.dao.user.UserStatisticsDaoImpl;
 import com.pulsar.habitica.dto.LoginUserDto;
+import com.pulsar.habitica.dto.ProfileUserDto;
 import com.pulsar.habitica.dto.RegisterUserDto;
 import com.pulsar.habitica.dto.UserDto;
 import com.pulsar.habitica.entity.user.User;
@@ -14,6 +17,8 @@ import com.pulsar.habitica.validation.LoginValidator;
 import com.pulsar.habitica.validation.RegisterValidator;
 import com.pulsar.habitica.validation.Validator;
 
+import java.io.IOException;
+
 public class UserService {
 
     private final UserDao userDao;
@@ -22,23 +27,40 @@ public class UserService {
     private final Mapper<RegisterUserDto, User> registerUserMapper = RegisterUserMapper.getInstance();
     private final Mapper<User, UserDto> userMapper = UserMapper.getInstance();
     private final UserBalanceService userBalanceService;
+    private final UserImageService userImageService;
+    private final UserStatisticsService userStatisticsService;
+
+    {
+        this.userBalanceService = new UserBalanceService(UserBalanceDaoImpl.getInstance());
+        this.userImageService = new UserImageService(UserImageDaoImpl.getInstance());
+        this.userStatisticsService = new UserStatisticsService(UserStatisticsDaoImpl.getInstance());
+    }
 
     public UserService(UserDao userDao) {
         this.userDao = userDao;
         this.registerValidator = new RegisterValidator(userDao);
         this.loginValidator = new LoginValidator(userDao);
-        this.userBalanceService = new UserBalanceService(UserBalanceDaoImpl.getInstance());
     }
 
-    public UserDto create(RegisterUserDto registerUserDto) {
+    public ProfileUserDto create(RegisterUserDto registerUserDto) throws IOException {
         var validationResult = registerValidator.isValid(registerUserDto);
         if (validationResult.isInvalid()) {
             throw new ValidationException(validationResult.getErrors());
         }
-        User user = registerUserMapper.mapFrom(registerUserDto);
+        var user = registerUserMapper.mapFrom(registerUserDto);
+
         user = userDao.save(user);
-        userBalanceService.initUserBalance(user);
-        return userMapper.mapFrom(user);
+
+        var userBalance = userBalanceService.initUserBalance(user.getId());
+        var userImage = userImageService.initUserImage(user.getId());
+        var userStatistics = userStatisticsService.initUserStatistics(user.getId());
+
+        return ProfileUserDto.builder()
+                .userDto(userMapper.mapFrom(user))
+                .userBalance(userBalance)
+                .userImage(userImage)
+                .userStatistics(userStatistics)
+                .build();
     }
 
     public UserDto login(LoginUserDto loginUserDto) {
