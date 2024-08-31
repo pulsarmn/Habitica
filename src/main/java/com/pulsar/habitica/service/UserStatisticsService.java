216e7a215f2657f82237bccff0a36b4eac44d611
Кузичев Александр <pulsarmn@yandex.ru>
2024-08-31 15:36:10 +0300

    add: implement the initUserStatistics() method

--- src/main/java/com/pulsar/habitica/service/UserStatisticsService.java
+++ src/main/java/com/pulsar/habitica/service/UserStatisticsService.java
@@ -1,6 +1,8 @@
 package com.pulsar.habitica.service;
 
 import com.pulsar.habitica.dao.user.UserStatisticsDao;
+import com.pulsar.habitica.entity.user.User;
+import com.pulsar.habitica.entity.user.UserStatistics;
 
 public class UserStatisticsService {
 
@@ -8,5 +10,12 @@ public class UserStatisticsService {
 
     public UserStatisticsService(UserStatisticsDao userStatisticsDao) {
         this.userStatisticsDao = userStatisticsDao;
+    }
+
+    public UserStatistics initUserStatistics(int userId) {
+        return userStatisticsDao.save(UserStatistics.builder()
+                .userId(userId)
+                .totalVisits(0)
+                .build());
     }
 }
