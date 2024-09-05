905acd9c86b285dcd5f4e4a41151aa94c9115ee9
Кузичев Александр <pulsarmn@yandex.ru>
2024-09-05 09:35:29 +0300

    add: implement the addUserToSession() method

--- src/main/java/com/pulsar/habitica/servlet/HomeServlet.java
+++ src/main/java/com/pulsar/habitica/servlet/HomeServlet.java
@@ -1,5 +1,9 @@
 package com.pulsar.habitica.servlet;
 
+import com.pulsar.habitica.dao.user.UserDaoImpl;
+import com.pulsar.habitica.dto.ProfileUserDto;
+import com.pulsar.habitica.dto.UserDto;
+import com.pulsar.habitica.service.UserService;
 import com.pulsar.habitica.util.JspHelper;
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
@@ -13,19 +17,32 @@ import java.io.IOException;
 @WebServlet("/home")
 public class HomeServlet extends HttpServlet {
 
+    private UserService userService;
+
     @Override
     public void init(ServletConfig config) throws ServletException {
-
+        var userDao = UserDaoImpl.getInstance();
+        userService = new UserService(userDao);
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        var user = (UserDto) request.getSession().getAttribute("user");
+        var profileUserDto = userService.getProfileUserDto(user.getId());
+        addUserToSession(request, profileUserDto);
         request.getRequestDispatcher(JspHelper.getPath("home")).forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+    }
+
+    private void addUserToSession(HttpServletRequest request, ProfileUserDto profileUser) {
+        request.getSession().setAttribute("user", profileUser.getUserDto());
+        request.getSession().setAttribute("userBalance", profileUser.getUserBalance());
+        request.getSession().setAttribute("userImage", profileUser.getUserImage());
+        request.getSession().setAttribute("userStatistics", profileUser.getUserStatistics());
     }
 
     @Override
