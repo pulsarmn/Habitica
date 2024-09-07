f99284fdce4bc16e72e464d92875420b063c889a
Кузичев Александр <pulsarmn@yandex.ru>
2024-09-07 14:29:43 +0300

    add: create the TaskDtoMapper class and implement the mapFrom() method

---
+++ src/main/java/com/pulsar/habitica/mapper/TaskDtoMapper.java
@@ -0,0 +1,31 @@
+package com.pulsar.habitica.mapper;
+
+import com.pulsar.habitica.dto.TaskDto;
+import com.pulsar.habitica.entity.task.Task;
+
+public class TaskDtoMapper implements Mapper<TaskDto, Task> {
+
+    private static TaskDtoMapper INSTANCE;
+
+    private TaskDtoMapper() {}
+
+    @Override
+    public Task mapFrom(TaskDto taskDto) {
+        return Task.builder()
+                .userId(taskDto.getUserId())
+                .heading(taskDto.getHeading())
+                .status(false)
+                .build();
+    }
+
+    public static TaskDtoMapper getInstance() {
+        if (INSTANCE == null) {
+            synchronized (TaskDtoMapper.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new TaskDtoMapper();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
