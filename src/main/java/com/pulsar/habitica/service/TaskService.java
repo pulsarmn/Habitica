e652273cf8164f92777844ca171749c1f5bf9985
Кузичев Александр <pulsarmn@yandex.ru>
2024-09-07 14:33:16 +0300

    add: implement the createTask() method in the TaskService class

--- src/main/java/com/pulsar/habitica/service/TaskService.java
+++ src/main/java/com/pulsar/habitica/service/TaskService.java
@@ -1,13 +1,22 @@
 package com.pulsar.habitica.service;
 
 import com.pulsar.habitica.dao.task.TaskDao;
+import com.pulsar.habitica.dto.TaskDto;
 import com.pulsar.habitica.entity.task.Task;
+import com.pulsar.habitica.mapper.Mapper;
+import com.pulsar.habitica.mapper.TaskDtoMapper;
 
 public class TaskService {
 
     private final TaskDao<Task> taskDao;
+    private final Mapper<TaskDto, Task> dtoTaskMapper = TaskDtoMapper.getInstance();
 
     public TaskService(TaskDao<Task> taskDao) {
         this.taskDao = taskDao;
+    }
+
+    public Task createTask(TaskDto taskDto) {
+        var task = dtoTaskMapper.mapFrom(taskDto);
+        return taskDao.save(task);
     }
 }
