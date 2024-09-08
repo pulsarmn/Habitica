025695d22d551c22d3570fec7553e75a3b2f6a54
Кузичев Александр <pulsarmn@yandex.ru>
2024-09-08 20:29:27 +0300

    add: dependency on TaskService in HomeServlet class

--- src/main/java/com/pulsar/habitica/servlet/HomeServlet.java
+++ src/main/java/com/pulsar/habitica/servlet/HomeServlet.java
@@ -1,8 +1,10 @@
 package com.pulsar.habitica.servlet;
 
+import com.pulsar.habitica.dao.task.TaskDaoImpl;
 import com.pulsar.habitica.dao.user.UserDaoImpl;
 import com.pulsar.habitica.dto.ProfileUserDto;
 import com.pulsar.habitica.dto.UserDto;
+import com.pulsar.habitica.service.TaskService;
 import com.pulsar.habitica.service.UserService;
 import com.pulsar.habitica.util.JspHelper;
 import jakarta.servlet.ServletConfig;
@@ -18,17 +20,22 @@ import java.io.IOException;
 public class HomeServlet extends HttpServlet {
 
     private UserService userService;
+    private TaskService taskService;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
         var userDao = UserDaoImpl.getInstance();
         userService = new UserService(userDao);
+        var taskDao = TaskDaoImpl.getInstance();
+        taskService = new TaskService(taskDao);
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         var user = (UserDto) request.getSession().getAttribute("user");
         var profileUserDto = userService.getProfileUserDto(user.getId());
+        var taskList = taskService.findAllByUserId(user.getId());
+        request.getSession().setAttribute("tasks", taskList);
         addUserToSession(request, profileUserDto);
         request.getRequestDispatcher(JspHelper.getPath("home")).forward(request, response);
     }
