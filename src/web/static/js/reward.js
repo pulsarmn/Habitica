fee4b5c41917c9facdea50caeb906c7f1c2e1195
Кузичев Александр <pulsarmn@yandex.ru>
2024-09-17 19:50:05 +0300

    add: create the reward.js file

---
+++ src/web/static/js/reward.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const taskInput = document.getElementById('rewardInput');
+    taskInput.addEventListener('keypress', function (e) {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            const taskValue = taskInput.value.trim();
+            if (taskValue) {
+                fetch('/reward', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                    },
+                    body: `rewardHeading=${encodeURIComponent(taskValue)}`
+                })
+                    .then(response => {
+                        if (response.ok) {
+                            taskInput.value = '';
+                            console.assert('Награда отправлена успешно!');
+                        } else {
+                            console.assert('Ошибка при отправке награды!');
+                        }
+                    })
+                    .catch(error => {
+                        console.assert('Ошибка сети: ' + error.message);
+                    });
+            }
+        }
+    });
+
+    taskInput.addEventListener('input', function () {
+        if (this.value.includes('\n')) {
+            this.value = this.value.replace(/\n/g, '');
+        }
+    });
+});
