abbb85d7ae3c7cbf26b0b6614cf95581d8b945f0
Кузичев Александр <pulsarmn@yandex.ru>
2024-09-17 15:14:18 +0300

    add: implement the createHabit() method

--- src/main/java/com/pulsar/habitica/service/HabitService.java
+++ src/main/java/com/pulsar/habitica/service/HabitService.java
@@ -1,13 +1,22 @@
 package com.pulsar.habitica.service;
 
 import com.pulsar.habitica.dao.task.TaskDao;
+import com.pulsar.habitica.dto.TaskDto;
 import com.pulsar.habitica.entity.task.Habit;
+import com.pulsar.habitica.mapper.HabitDtoMapper;
+import com.pulsar.habitica.mapper.Mapper;
 
 public class HabitService {
 
     private TaskDao<Habit> habitDao;
+    private Mapper<TaskDto, Habit> dtoHabitMapper = HabitDtoMapper.getInstance();
 
     public HabitService(TaskDao<Habit> habitDao) {
         this.habitDao = habitDao;
+    }
+
+    public Habit createHabit(TaskDto taskDto) {
+        var habit = dtoHabitMapper.mapFrom(taskDto);
+        return habitDao.save(habit);
     }
 }
