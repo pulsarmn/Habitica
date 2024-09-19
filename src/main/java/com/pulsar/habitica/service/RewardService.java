e3c93f7bd11f6cd8efe9c765f684120b2a5b8410
Кузичев Александр <pulsarmn@yandex.ru>
2024-09-19 20:25:06 +0300

    add: implement the findById() method

--- src/main/java/com/pulsar/habitica/service/RewardService.java
+++ src/main/java/com/pulsar/habitica/service/RewardService.java
@@ -1,6 +1,7 @@
 package com.pulsar.habitica.service;
 
 import com.pulsar.habitica.dao.reward.RewardDao;
+import com.pulsar.habitica.entity.Reward;
 
 public class RewardService {
 
@@ -8,5 +9,10 @@ public class RewardService {
 
     public RewardService(RewardDao rewardDao) {
         this.rewardDao = rewardDao;
+    }
+
+    public Reward findById(int rewardId) {
+        return rewardDao.findById(rewardId)
+                .orElse(Reward.builder().build());
     }
 }
