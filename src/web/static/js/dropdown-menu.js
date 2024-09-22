56be0b28f434de1fb0244d8dec2109010a9b8c9b
Кузичев Александр <pulsarmn@yandex.ru>
2024-09-22 19:06:37 +0300

    add: implement the delete button in the task drop-down menu

--- src/web/static/js/dropdown-menu.js
+++ src/web/static/js/dropdown-menu.js
@@ -14,3 +14,31 @@ document.addEventListener('click', function(event) {
         });
     }
 });
+
+document.getElementById('tasks-container').addEventListener('click', function(event) {
+    if (event.target.closest('.habitica-menu-dropdown-toggle')) {
+        const dropdownToggle = event.target.closest('.habitica-menu-dropdown-toggle');
+        const taskWrapper = dropdownToggle.closest('.task-wrapper');
+        const dropdownMenu = taskWrapper.querySelector('.dropdown-menu');
+        const taskId = taskWrapper.querySelector('.task-id').textContent;
+
+        dropdownMenu.addEventListener('click', function(event) {
+            if (event.target.closest('.delete-task-item')) {
+                fetch(`/tasks?taskId=${taskId}`, {
+                    method: 'DELETE'
+                }).then(response => {
+                    if (response.ok) {
+                        console.log(`Задача с ID ${taskId} удалена`);
+                        updateTasks();
+                    } else {
+                        console.error('Ошибка при удалении задачи');
+                    }
+                }).catch(error => {
+                    console.error('Ошибка сети:', error);
+                });
+            }else if (event.target.closest('.edit-task-item')) {
+
+            }
+        });
+    }
+});
