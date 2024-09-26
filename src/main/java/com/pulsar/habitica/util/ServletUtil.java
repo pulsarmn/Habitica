1a02c6ad5b276832cf6a61e756a3586140fea19a
Кузичев Александр <pulsarmn@yandex.ru>
2024-09-26 20:31:39 +0300

    add: create the ServletUtil class

---
+++ src/main/java/com/pulsar/habitica/util/ServletUtil.java
@@ -0,0 +1,16 @@
+package com.pulsar.habitica.util;
+
+import com.pulsar.habitica.dto.UserDto;
+import com.pulsar.habitica.exception.UnauthorizedException;
+import com.pulsar.habitica.servlet.SessionAttribute;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+public final class ServletUtil {
+
+    private ServletUtil() {}
+}
