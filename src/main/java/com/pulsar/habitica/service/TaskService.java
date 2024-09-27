4af047da44607cdcf59f37d6cc586bb2cffaad97
Кузичев Александр <pulsarmn@yandex.ru>
2024-09-27 10:15:18 +0300

    add: implement the updateTask() method in the TaskService class

--- src/main/java/com/pulsar/habitica/service/TaskService.java
+++ src/main/java/com/pulsar/habitica/service/TaskService.java
@@ -2,10 +2,16 @@ package com.pulsar.habitica.service;
 
 import com.pulsar.habitica.dao.task.TaskDao;
 import com.pulsar.habitica.dto.TaskDto;
+import com.pulsar.habitica.entity.task.Complexity;
 import com.pulsar.habitica.entity.task.Task;
+import com.pulsar.habitica.exception.UnauthorizedException;
 import com.pulsar.habitica.mapper.Mapper;
 import com.pulsar.habitica.mapper.TaskDtoMapper;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -30,6 +36,33 @@ public class TaskService {
 
     public List<Task> findAllByUserId(int userId) {
         return taskDao.findAllByUserId(userId);
+    }
+
+    public Task updateTask(int userId, JSONObject taskData) {
+        if (taskData == null || taskData.isEmpty()) {
+            throw new JSONException("JSON is invalid or empty");
+        }
+
+        int taskId = taskData.getInt("id");
+        Task task = findById(taskId);
+
+        if (task.getUserId() != userId) {
+            throw new UnauthorizedException();
+        }
+
+        LocalDate taskDeadline;
+        try {
+            taskDeadline = LocalDate.parse(taskData.getString("deadline"));
+        }catch (DateTimeParseException e) {
+            taskDeadline = null;
+        }
+
+        task.setHeading(taskData.getString("heading"));
+        task.setDescription(taskData.getString("description"));
+        task.setComplexity(Complexity.valueOf(taskData.getString("complexity")));
+        task.setDeadline(taskDeadline);
+
+        return taskDao.update(task);
     }
 
     public boolean deleteTask(int taskId) {
