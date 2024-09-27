c4a5723b8784ee0d2734fd76dedb57d3657c4ab6
Кузичев Александр <pulsarmn@yandex.ru>
2024-09-27 10:15:58 +0300

    add: implement the doPut() method in the TaskServlet class

--- src/main/java/com/pulsar/habitica/servlet/TaskServlet.java
+++ src/main/java/com/pulsar/habitica/servlet/TaskServlet.java
@@ -4,18 +4,22 @@ import com.pulsar.habitica.dao.task.TaskDao;
 import com.pulsar.habitica.dao.task.TaskDaoImpl;
 import com.pulsar.habitica.dto.TaskDto;
 import com.pulsar.habitica.dto.UserDto;
+import com.pulsar.habitica.entity.task.Complexity;
 import com.pulsar.habitica.entity.task.Task;
 import com.pulsar.habitica.filter.PrivatePaths;
 import com.pulsar.habitica.service.TaskService;
 import com.pulsar.habitica.util.JspHelper;
+import com.pulsar.habitica.util.ServletUtil;
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,10 +41,25 @@ public class TaskServlet extends HttpServlet {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             return;
         }
-        List<Task> tasks = taskService.findAllByUserId(user.getId());
+        var taskId = request.getParameter("id");
+        if (taskId == null) {
+            List<Task> tasks = taskService.findAllByUserId(user.getId());
 
-        request.setAttribute(SessionAttribute.TASKS.getValue(), tasks);
-        request.getRequestDispatcher(JspHelper.getPath(PrivatePaths.TASKS.getPath())).include(request, response);
+            request.setAttribute(SessionAttribute.TASKS.getValue(), tasks);
+            request.getRequestDispatcher(JspHelper.getPath(PrivatePaths.TASKS.getPath())).include(request, response);
+        }else {
+            int id;
+            try {
+                id = Integer.parseInt(taskId);
+            }catch (NumberFormatException e) {
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                return;
+            }
+            var task = taskService.findById(id);
+            request.setAttribute("task", task);
+            request.setAttribute("taskData", new JSONObject(task));
+            request.getRequestDispatcher(JspHelper.getPath("/task-modal-window")).include(request, response);
+        }
     }
 
     @Override
@@ -57,15 +76,21 @@ public class TaskServlet extends HttpServlet {
     @Override
     protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         var user = (UserDto) request.getSession().getAttribute(SessionAttribute.USER.getValue());
-        var id = request.getParameter("taskId");
+        var id = request.getParameter("id");
         int taskId = id.matches("\\d+") ? Integer.parseInt(id) : 0;
 
         var result = taskService.deleteTask(taskId);
     }
 
     @Override
-    public void destroy() {
-
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            var user = ServletUtil.getAuthenticatedUser(request);
+            JSONObject jsonTask = ServletUtil.getJson(request);
+            taskService.updateTask(user.getId(), jsonTask);
+        }catch (Exception e) {
+            ServletUtil.handleException(response, e);
+        }
     }
 
     private List<Task> getTaskList(HttpServletRequest request) {
